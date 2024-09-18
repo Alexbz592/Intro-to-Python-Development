34ac0a6f1c11e9789d94c200e7eb7368a379e959
Alexandru Bujor <alexandru.bujor@cnmv.ro>
2024-09-18 21:01:48 +0300

    curs02

---
+++ curs02/curs_for.py
@@ -0,0 +1,12 @@
+lista = [41, 42, 43]
+# i = 'seara'
+# lista = 'Ana'
+# for i, v in enumerate(lista):
+    # print(i, v)
+# print(i)
+# dictionar = {'a': 1, 'b': 2}
+# for key, value in dictionar.items() :
+    # print(key, value)
+for i in lista :
+    if i % 2 != 0:
+        print(i)
